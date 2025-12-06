77cab1d64969760cde7bf28e8b126994836c2214
Cane Gao <caneg@ciandt.com>
2025-12-06 13:46:23 +0800

    Ini dev.

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -897,3 +897,8 @@ if (getenv('IS_DDEV_PROJECT') == 'true' && file_exists(__DIR__ . '/settings.ddev
 # if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
+
+/**
+ * Location of the site configuration files.
+ */
+$settings['config_sync_directory'] = '../config/sync';
